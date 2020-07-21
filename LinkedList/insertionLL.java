3aee087ddabd2dc74edb363fa1a3aad4bea83544
Shouvik <49680243+shouvik78@users.noreply.github.com>
2020-07-21 23:40:57 +0530

    21 july

--- LinkedList/insertionLL.java
+++ LinkedList/insertionLL.java
@@ -27,6 +27,7 @@ public class insertionLL {
 	
 	static Node1 insertAtHead(Node1 head, int data)
 	{
+		
 		Node1 newNode= new Node1(data);
 		newNode.next=head;
 		head=newNode;
@@ -34,17 +35,42 @@ public class insertionLL {
 		return head;
 	}
 	
+	static void insertAtEnd(Node1 head, int data)
+	{
+		/*if(head==null)
+		{
+			Node1 newNode=new Node1(data);
+			head=newNode;
+			return;
+		}*/
+		Node1 newNode=new Node1(data);
+		while(head.next!=null)
+		{
+			head=head.next;
+		}
+		head.next=newNode;
+		//return head;
+	}
+	
 
 	public static void main(String[] args) {
 		{
 			// TODO Auto-generated method stub
+//			Node1 head=new Node1(5);
+//			head.next=new Node1(6);
+//			head.next.next=new Node1(7);
+//			head.next.next.next=new Node1(8);
+//			head.next.next.next.next=new Node1(9);
 			Node1 head=new Node1(5);
-			head.next=new Node1(6);
-			head.next.next=new Node1(7);
-			head.next.next.next=new Node1(8);
-			head.next.next.next.next=new Node1(9);
+			insertAtEnd(head,1);
+			insertAtEnd(head,2);
+			insertAtEnd(head,3);
+			insertAtEnd(head,4);
+			
 			print(head);
 			head=insertAtHead(head,4);
+			print(head);
+			insertAtEnd(head,10);
 			print(head);
 		}
 	}
