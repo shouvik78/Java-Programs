602faf0974cc05a018ef55040cda2471f27eb54b
Shouvik <49680243+shouvik78@users.noreply.github.com>
2020-07-29 23:20:39 +0530

    29 july

---
+++ trees/BinaryTree.java
@@ -0,0 +1,81 @@
+package trees;
+
+class Node
+{
+	int data;
+	Node left;
+	Node right;
+	Node(int data)
+	{
+		this.data=data;
+	}
+}
+
+public class BinaryTree {
+	Node root;
+	BinaryTree()
+	{
+		root=null;
+	}
+	
+	BinaryTree(int data)
+	{
+		this.root=new Node(data);
+	}
+	
+	int TreeSum(Node root)
+	{
+		if(root==null)
+			return 0;
+		return root.data+TreeSum(root.left)+TreeSum(root.right);
+	}
+	
+	int countNodes(Node root)
+	{
+		if(root==null) return 0;
+		return 1+countNodes(root.left)+countNodes(root.right);
+	}
+	
+	int countLeaf(Node root)
+	{
+		if(root==null) return 0;
+		if(root.left==null && root.right==null) return 1;
+		
+		return countLeaf(root.left)+countLeaf(root.right);
+	}
+	int height(Node root) 
+    {
+         if(root==null) return -1;
+         return 1+Math.max(height(root.left),height(root.right));
+    }
+	
+	void printLevel(Node root, int level)
+	{
+		if(root==null) return;
+		if(level==0)
+		{
+			System.out.print(root.data+" ");
+			return;
+		}
+		printLevel(root.left,level-1);
+		printLevel(root.right,level-1);
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		BinaryTree b=new BinaryTree(2);
+		b.root.left=new Node(3);
+		b.root.right=new Node(5);
+		b.root.left.right=new Node(9);
+		b.root.right.left=new Node(7);
+		System.out.println(b.TreeSum(b.root));
+		//System.out.println(b.root.data);
+		System.out.println(b.countNodes(b.root));
+		System.out.println(b.countLeaf(b.root));
+		System.out.print("height is ");
+		System.out.println(b.height(b.root));
+		b.printLevel(b.root, 2);
+	}
+
+}
