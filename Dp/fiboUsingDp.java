fd42c826ece2b51eb7bdc5b084dcfd83a7d03bef
Shouvik <49680243+shouvik78@users.noreply.github.com>
2020-08-09 23:12:56 +0530

    Dp programs

---
+++ Dp/fiboUsingDp.java
@@ -0,0 +1,32 @@
+package Dp;
+
+public class fiboUsingDp {
+	static int fib(int n)
+	{
+		if(n==0 || n==1) return n;
+		return fib(n-1)+fib(n-2);
+	}
+	
+	static int fibDp(int n)
+	{
+		int storage[]=new int[n+1];
+		storage[0]=0;
+		storage[1]=1;
+		for(int i=2;i<=n;i++)
+		{
+			storage[i]=storage[i-1]+storage[i-2];
+		}
+		return storage[n];
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int n=45;
+		//System.out.println(fib(n));
+		System.out.println(fibDp(n));
+		
+	}
+
+}
---
+++ Dp/stairDp.java
@@ -0,0 +1,31 @@
+package Dp;
+
+public class stairDp {
+	static int stairRec(int n)
+	{
+		if(n==0) return 1;
+		if(n==1 || n==2) return n;
+		return stairRec(n-1)+stairRec(n-2)+stairRec(n-3);
+	}
+	static int stair(int n)
+	{
+		int store[]=new int[n+1];
+		store[0]=1;
+		store[1]=1;
+		store[2]=2;
+		for(int i=3;i<=n;i++)
+		{
+			store[i]=store[i-1]+store[i-2]+store[i-3];
+		}
+		return store[n];
+	}
+
+	public static void main(String[] args) {
+		
+		int n=35;
+		System.out.println(stair(n));
+		System.out.println(stairRec(n));
+		
+	}
+
+}
