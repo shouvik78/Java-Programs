2a13ee8a224a714ca47ff83254c70c147844965a
Shouvik <49680243+shouvik78@users.noreply.github.com>
2020-08-11 22:32:26 +0530

    username

---
+++ stringss/usernameValid.java
@@ -0,0 +1,25 @@
+package stringss;
+
+import java.util.Scanner;
+class UsernameValidator 
+{
+ 
+    public static final String regularExpression = "^[a-zA-Z]\\w{7,29}$";
+}
+
+	class usernameValid {
+    private static final Scanner scan = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        int n = Integer.parseInt(scan.nextLine());
+        while (n-- != 0) {
+            String userName = scan.nextLine();
+
+            if (userName.matches(UsernameValidator.regularExpression)) {
+                System.out.println("Valid");
+            } else {
+                System.out.println("Invalid");
+            }           
+        }
+    }
+}
