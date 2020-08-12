a940e12a5380c3dee53a90b6faf51a8edc837083
Shouvik <49680243+shouvik78@users.noreply.github.com>
2020-08-12 13:37:05 +0530

    sha256

---
+++ cryptography/SHA256.java
@@ -0,0 +1,34 @@
+package cryptography;
+import java.io.*;
+import java.util.*;
+import java.security.MessageDigest;
+
+
+
+public class SHA256 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner in = new Scanner(System.in);
+        String message = in.nextLine();
+
+        try 
+        {
+            MessageDigest messageDigest 
+                = MessageDigest.getInstance("SHA-256");
+            messageDigest.update(message.getBytes());
+            byte[] hash = messageDigest.digest();
+
+            for (byte b : hash) 
+            {
+                System.out.printf("%02x", b);
+            }
+        } 
+        catch (Exception e) {
+        	
+        }
+
+	}
+
+}
