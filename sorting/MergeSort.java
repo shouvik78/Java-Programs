8759ec6a98adae730dd1b9e95fa7c78e3114e724
Shouvik <49680243+shouvik78@users.noreply.github.com>
2020-08-13 23:21:13 +0530

    13 aug

--- sorting/MergeSort.java
+++ sorting/MergeSort.java
@@ -22,7 +22,7 @@ class MergeSort {
   
         /* Merge the temp arrays */
   
-        // Initial indexes of first and second subarrays 
+        // Initial indexes of first and second sub arrays 
         int i = 0, j = 0; 
   
         // Initial index of merged subarry array 
