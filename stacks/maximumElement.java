95f9c6e670e51a26c50f588d037806d742a2b0e3
Shouvik <49680243+shouvik78@users.noreply.github.com>
2020-08-15 22:15:24 +0530

    15 aug

---
+++ stacks/maximumElement.java
@@ -0,0 +1,55 @@
+package stacks;
+	import java.io.*;
+	import java.util.*;
+
+	public class maximumElement {
+
+	    public static void main(String[] args) {
+	        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+	        
+	        Scanner sc = new Scanner(System.in);
+	    int n = sc.nextInt();
+	    int max = Integer.MIN_VALUE;
+	    Stack<StackNode> stack = new Stack<StackNode>();
+
+	    while (n > 0) {
+	        int choice = sc.nextInt();
+	        if(choice == 1) {
+	            int val = sc.nextInt();
+	            max = Math.max(val, max);
+
+	            stack.add(new StackNode(val, max));
+	        } else if(choice == 2) {
+	            if(!stack.isEmpty())
+	                stack.pop();
+	            // reset max
+	            if(stack.isEmpty())
+	                max = Integer.MIN_VALUE;
+	            else
+	                max = stack.peek().curMax;
+	        } else if(choice == 3) {
+	            if(!stack.isEmpty()) {
+	                System.out.println(stack.peek().curMax);
+	            }
+	        }
+
+	        n--;
+	    }
+	    sc.close();
+	}
+
+	private static class StackNode {
+	    int val;
+	    int curMax;
+	    public StackNode(int val, int curMax) {
+	        this.val = val;
+	        this.curMax = curMax;
+	    }
+
+	    public String toString() {
+	        return val + " [" + curMax + "]";
+	    }
+	    }
+	}
+
+
