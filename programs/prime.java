5b3dd06bbd92387447ed08cca24e3fd5c54c8f54
Shouvik <49680243+shouvik78@users.noreply.github.com>
2020-08-22 22:28:36 +0530

    22 aug

--- programs/prime.java
+++ programs/prime.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class prime {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated 
 		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
 		int count=0;
