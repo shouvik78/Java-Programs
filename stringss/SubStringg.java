7c462dbf7b81ae07f2aa5ccfc5f74c9f11b5f357
Shouvik <49680243+shouvik78@users.noreply.github.com>
2020-08-29 22:29:47 +0530

    29 aug

--- stringss/SubStringg.java
+++ stringss/SubStringg.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class SubStringg {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Scanner sc=new Scanner(System.in);
 		String s=sc.next();
 		int start=sc.nextInt();
