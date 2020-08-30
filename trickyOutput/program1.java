a52751ab0c38f659ad7923b195232a249e18cf49
Shouvik <49680243+shouvik78@users.noreply.github.com>
2020-08-30 22:06:53 +0530

    30 aug

--- trickyOutput/program1.java
+++ trickyOutput/program1.java
@@ -3,7 +3,7 @@ package trickyOutput;
 public class program1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		// the line below this gives an output 
         // \u000d System.out.println("comment executed");
 
