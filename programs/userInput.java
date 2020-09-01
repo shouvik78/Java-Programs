9cee5d8e6095f791d78a9e06e963efd51a742ad3
Shouvik <49680243+shouvik78@users.noreply.github.com>
2020-09-01 20:03:40 +0530

    1st sep

--- programs/userInput.java
+++ programs/userInput.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class userInput {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 		Scanner sc=new Scanner(System.in);
 		int x=sc.nextInt();
